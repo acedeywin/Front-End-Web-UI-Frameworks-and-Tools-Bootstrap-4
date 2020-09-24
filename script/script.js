c40776fc311037fca8bc9bd7d90fd66514a8acce
Stephen Okpalaononuju <stephenokpala@gmail.com>
2020-09-24 10:04:50 +0100

    Bootstrap jQuery

--- script/script.js
+++ script/script.js
@@ -1,3 +1,13 @@
 $(document).ready(function () {
-  $('[data-toggle="tooltip"]').tooltip();
+  // $('[data-toggle="tooltip"]').tooltip();
+
+  $("#mycarousel").carousel({ interval: 1000 });
+
+  $("#carousel-pause").click(function () {
+    $("#mycarousel").carousel("pause");
+  });
+
+  $("#carousel-play").click(function () {
+    $("#mycarousel").carousel("cycle");
+  });
 });
