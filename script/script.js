8dc48e5468cbc4c02c70a161a089786058c676b1
Stephen Okpalaononuju <stephenokpala@gmail.com>
2020-09-24 11:20:22 +0100

    More Bootstrap and jQuery

--- script/script.js
+++ script/script.js
@@ -1,13 +1,17 @@
 $(document).ready(function () {
   // $('[data-toggle="tooltip"]').tooltip();
 
-  $("#mycarousel").carousel({ interval: 1000 });
+  $("#mycarousel").carousel({ interval: 1000, pause: false });
 
-  $("#carousel-pause").click(function () {
-    $("#mycarousel").carousel("pause");
-  });
-
-  $("#carousel-play").click(function () {
-    $("#mycarousel").carousel("cycle");
+  $("#carouselButton").click(function () {
+    if ($("#carouselButton").children("span").hasClass("fa-pause")) {
+      $("#mycarousel").carousel("pause");
+      $("#carouselButton").children("span").removeClass("fa-pause");
+      $("#carouselButton").children("span").addClass("fa-play");
+    } else if ($("#carouselButton").children("span").hasClass("fa-play")) {
+      $("#mycarousel").carousel("cycle");
+      $("#carouselButton").children("span").removeClass("fa-play");
+      $("#carouselButton").children("span").addClass("fa-pause");
+    }
   });
 });
